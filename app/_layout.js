02db0c55760ffd93b96a5d40b7c0be6362468eb7
Judah40 <judahdore@gmail.com>
2023-09-14 02:04:12 +0000

    first commit

--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,10 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
+import { Slot, SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import { SessionProvider } from '../ctx';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -20,8 +21,8 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    "Inter-SemiBoldItalic":
+    "https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12",
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -46,11 +47,8 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+   <SessionProvider>
+    <Slot/>
+   </SessionProvider>
   );
 }
