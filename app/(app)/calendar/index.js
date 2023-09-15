3a6116e1dd53a2635a3f6ea92ea455b0ccbe2f67
Judah40 <judahdore@gmail.com>
2023-09-15 13:46:34 +0000

    date sixty percent

--- app/(app)/calendar/index.js
+++ app/(app)/calendar/index.js
@@ -12,19 +12,24 @@ const calendarMain = () => {
 
   // state
   const [selected, setSelected] = useState({
-    '2023-09-15': [{ name: 'Event 1' }],
-    '2023-09-16': [{ name: 'Event 2' }],
+    '2023-09-15': [{ name: 'Event 1' }, {description:'Apointment with Doctor Nahim'}],
+    '2023-09-16': [{ name: 'Event 2' }, {description:'Apointment with Doctor Brima'}],
     // Add more items for other dates
   });
 
   return (
-    <View className="flex-1 items-center">
+    <View className="flex-1 items-center ">
       <Agenda
       style={{width:400}}
       items={selected}
       renderItem={(item)=>(
-        <View>
-        
+        <View className="pt-4">
+        <Text className="text-xl font-bold text-gray-500 ">
+        {item.name}
+        </Text>
+        <Text className="">
+        {item.description}
+        </Text>
         </View>
       )}
       />
